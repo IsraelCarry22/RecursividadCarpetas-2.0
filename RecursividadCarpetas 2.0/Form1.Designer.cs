73cd098e5d55e942dfa570b43fd8adfd2f68b5d0
Israe <Israe@ISRAEL>
2023-08-31 15:04:39 -0600

    Agregar archivos de proyecto.

---
+++ RecursividadCarpetas 2.0/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace RecursividadCarpetas_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir directorio";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(213, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(386, 508);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 540);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ RecursividadCarpetas 2.0/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecursividadCarpetas_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            using (var SeleccionaCarpeta = new FolderBrowserDialog())
+            {
+                if (SeleccionaCarpeta.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                string RutaCarpeta = SeleccionaCarpeta.SelectedPath;
+                listBox1.Items.Clear();
+                ListaRecursividadCarpetas(RutaCarpeta);
+            }
+        }
+
+        public void ListaRecursividadCarpetas(string carpetas)
+        {
+            DirectoryInfo directory = new DirectoryInfo(carpetas);
+            foreach (FileInfo Archivo in directory.GetFiles())
+            {
+                string nombre = Archivo.Name;
+                string extension = Archivo.Extension;
+                listBox1.Items.Add($"Nom: {nombre} || extension: {extension}.");
+            }
+            foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+            {
+                listBox1.Items.Add($"Subcarpeta: {subdirectory.Name}");
+                ListaRecursividadCarpetas(subdirectory.FullName);
+            }
+        }
+    }
+}
